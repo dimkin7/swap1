878ad1095afbc7c4cfef6c10bbb938f166c4a92a
Dmitry <dima-besh@yandex.ru>
2022-03-09 00:05:20 +0300

    Таски дописаны.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,63 @@ import "solidity-coverage";
 
 dotenv.config();
 
+//это контракт стейкинга
+const contractAddr = '0xC55D91Cc3A1a574bd66A890Ea9f9477eD5dC7d7d'; 
+
+//таски на stake, unstake, claim
+
+// stake(uint256 amount) - списывает с пользователя на контракт стейкинга ЛП токены в количестве amount, обновляет в контракте баланс пользователя
+//npx hardhat stake --network rinkeby --key PRIVATE_KEY --amount 321
+task("stake", "stake")
+  .addParam("key", "Your private key")
+  .addParam("amount", "amount")
+  .setAction(async (taskArgs, hre) => {
+
+    const abi = [
+      "function stake(uint256 _amount) public"
+    ];
+    const provider = new hre.ethers.providers.AlchemyProvider("rinkeby");
+    const signer = new hre.ethers.Wallet(taskArgs.key, provider);
+    const contr = new hre.ethers.Contract(contractAddr, abi, signer);
+    
+    let success = await contr.stake(taskArgs.amount);
+    console.log('stake: ', success);
+  });
+
+// claim() - списывает с контракта стейкинга ревард токены доступные в качестве наград
+//npx hardhat claim --network rinkeby --key PRIVATE_KEY 
+task("claim", "claim")
+  .addParam("key", "Your private key")
+  .setAction(async (taskArgs, hre) => {
+
+    const abi = [
+      "function claim() public"
+    ];
+    const provider = new hre.ethers.providers.AlchemyProvider("rinkeby");
+    const signer = new hre.ethers.Wallet(taskArgs.key, provider);
+    const contr = new hre.ethers.Contract(contractAddr, abi, signer);
+    
+    let success = await contr.claim();
+    console.log('claim: ', success);
+  });
+
+// unstake() - списывает с контракта стейкинга ЛП токены доступные для вывода
+//npx hardhat unstake --network rinkeby --key PRIVATE_KEY 
+task("unstake", "unstake")
+  .addParam("key", "Your private key")
+  .setAction(async (taskArgs, hre) => {
+
+    const abi = [
+      "function unstake() public"
+    ];
+    const provider = new hre.ethers.providers.AlchemyProvider("rinkeby");
+    const signer = new hre.ethers.Wallet(taskArgs.key, provider);
+    const contr = new hre.ethers.Contract(contractAddr, abi, signer);
+    
+    let success = await contr.unstake();
+    console.log('unstake: ', success);
+  });
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -32,9 +89,9 @@ const config: HardhatUserConfig = {
     },
 
     //workaround for coverage
-    hardhat: {
+    /*hardhat: {
       initialBaseFeePerGas: 0
-    },
+    },*/
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
